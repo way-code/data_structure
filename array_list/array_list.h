234d6994e27f582d6b5b0ec27dfc8d0274d9376b
waycode <griiim134@gmail.com>
2023-02-14 18:47:57 +0900

    array_list

---
+++ array_list/array_list.h
@@ -0,0 +1,27 @@
+#ifndef ARRAY_LIST
+#define ARRAY_LIST
+
+#include <stdio.h>
+#define ARRAY_SIZE 1024
+
+typedef unsigned int data_t;
+
+typedef struct
+{
+    data_t array[ARRAY_SIZE];
+    size_t size;
+} list_t;
+
+typedef list_t* List_t;
+
+void init_list(List_t plist);
+void show_list(List_t plist);
+int add_data(List_t plist, data_t data);
+int insert_data(List_t plist, data_t data, size_t index);
+size_t search_data(List_t plist, data_t data);
+size_t count_data(List_t plist);
+data_t show_data(List_t plist, size_t index);
+data_t erase_data(List_t plist);
+data_t delete_data(List_t plist, size_t index);
+
+#endif
