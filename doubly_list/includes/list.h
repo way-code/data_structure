5ad0b711fc58526b3ec7dbe283836820ca7e130e
waycode <griiim134@gmail.com>
2023-02-26 15:58:07 +0900

    doubly_list

---
+++ doubly_list/includes/list.h
@@ -0,0 +1,36 @@
+#ifndef LIST_H
+#define LIST_H
+
+#include "data.h"
+
+typedef struct node
+{
+    data_t data;
+	struct node* prev;
+    struct node* next;
+}node_t;
+
+typedef struct list
+{
+    node_t* head;
+    node_t* tail;
+    node_t* curr;
+    int size;
+}list_t;
+
+typedef list_t* List_t;
+
+void put_list(List_t list);
+void free_list(List_t list);
+void move_curr(List_t list, int index);
+
+int insert_data(List_t list, int index, data_t data);
+int index_of_data(List_t list, data_t data);
+int count_data(List_t list);
+
+data_t delete_data(List_t list, int index);
+data_t extract_data(List_t list, int index);
+
+List_t new_list(void);
+
+#endif
---
+++ doubly_list/srcs/list.c
@@ -0,0 +1,131 @@
+#include "list.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void put_list(List_t list)
+{
+    list->curr = list->head->next;
+    printf("[ ");
+    while (list->curr->next != list->tail)
+    {
+        put_data(list->curr->data);
+        list->curr = list->curr->next;
+        putchar(',');
+    }
+    put_data(list->curr->data);
+    printf(" ]\n");
+}
+
+void free_list(List_t list)
+{
+    while (list->head->next != list->tail)
+    {
+        list->curr = list->head->next;
+        list->head->next = list->curr->next;
+        free(list->curr);
+    }
+    free(list->head);
+    free(list->tail);
+    free(list);
+}
+
+void move_curr(List_t list, int index)
+{
+    int i;
+
+    list->curr = list->head;
+    for (i = 0; i <= index; i++)
+    {
+        list->curr = list->curr->next;
+    }
+}
+
+int insert_data(List_t list, int index, data_t data)
+{
+    node_t* new_node = (node_t*)malloc(sizeof(node_t));
+
+    if (!new_node || index > list->size)
+    {
+        return (-1);
+    }
+    new_node->data = data;
+    move_curr(list, index - 1);
+    new_node->next = list->curr->next;
+	list->curr->next->prev = new_node;
+    list->curr->next = new_node;
+	new_node->prev = list->curr;
+    list->size++;
+    return (index);
+}
+
+int index_of_data(List_t list, data_t data)
+{
+    int idx;
+
+    list->curr = list->head->next;
+    for (idx = 0; idx < list->size; idx++)
+    {
+        if (list->curr->data == data)
+        {
+            return (idx);
+        }
+        list->curr = list->curr->next;
+    }
+    return (-1);
+}
+
+int count_data(List_t list)
+{
+	return (list->size);
+}
+
+data_t delete_data(List_t list, int index)
+{
+    data_t d_data;
+
+    if (list->head->next == list->tail || index >= list->size)
+    {
+        return (-1);
+    }
+    move_curr(list, index);
+    d_data = list->curr->data;
+    list->curr->prev->next = list->curr->next;
+	list->curr->next->prev = list->curr->prev;
+    free(list->curr);
+    list->size--;
+    return (d_data);
+}
+
+data_t extract_data(List_t list, int index)
+{
+    if (index >= list->size)
+    {
+        return (-1);
+    }
+    move_curr(list, index);
+    return (list->curr->data);
+}
+
+List_t new_list(void)
+{
+    List_t list = (List_t)malloc(sizeof(list_t));
+
+    if (list)
+    {
+        list->head = (node_t*)malloc(sizeof(node_t));
+        list->tail = (node_t*)malloc(sizeof(node_t));
+        if (!list->head || !list->tail)
+        {
+            return (NULL);
+        }
+        list->head->data = 0;
+		list->head->prev = NULL;
+        list->head->next = list->tail;
+        list->tail->data = 0;
+		list->tail->prev = list->head;
+        list->tail->next = NULL;
+        list->curr = NULL;
+        list->size = 0;
+    }
+    return (list);
+}
